a273e86ce3ee803fc5a4a0a8b5e874f316222608
Muhaz Ahmed <muhazvla313@gmail.com>
2023-07-07 18:06:51 +0530

    updated router and index.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Customer from "./pages/customer/Customer";
 import Home from "./pages/home/Home";
 import Navbar from "./layouts/Navbar/Navbar";
+import CaseOnHold from "./pages/CaseOnHold/CaseOnHold";
+import SaleOrder from "./pages/SaleOrder/SaleOrder";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/customer-data" element={<Customer />} />
+          <Route path="/case-on-hold" element={<CaseOnHold />} />
+          <Route path="/sale-order" element={<SaleOrder />} />
         </Routes>
       </BrowserRouter>
     </div>
